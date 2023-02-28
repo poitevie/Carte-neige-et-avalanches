3e487371084d4aa8de5d622e13354cf383f00141
Thomas Fournier <Thomas.Fournier2@etu.univ-grenoble-alpes.fr>
2023-02-28 15:43:30 +0100

    com

--- back/hgt/altitude.php
+++ back/hgt/altitude.php
@@ -6,20 +6,30 @@ header('Access-Control-Allow-Methods: GET, POST');
 header('Access-Control-Allow-Headers: X-Requested-With');
 
 create_folder("../" . $path_altitude);
+
+//Tableaux pour la génération des fichiers json contenant les informations des encadrements des massifs
 $cadrealpes = array();
 $cadrecorse = array();
 $cadrepyrenees = array();
 $cadremassif = array();
+
+//Récupération du fichier geojson avec toutes les délimitations des massifs
 if (!$jsontext = file_get_contents("../" . $path_geojson . "massifs.geojson"))
     die("Erreur : N'a pas pu ouvrir le fichier geojson des massifs");
 else {
     $json = json_decode($jsontext);
     $features = $json->{"features"};
 
+    //Parcours de tous les massifs
     for ($i = 0; $i < count($features); $i++) {
+        //id du massif (ex: 07 pour la chartreuse)
         $id = $features[$i]->{"properties"}->{"id"};
+
+        //Génération d'un fichier hgt par massif
         generatehgt($features[$i], $id);
     }
+
+    //Sauvegarde des fichiers
     $content = json_encode($cadrealpes);
     file_put_contents("../" . $path_geojson . "cadrealpes.json", $content);
     $content = json_encode($cadrecorse);
@@ -29,6 +39,8 @@ else {
     $content = json_encode($cadremassif);
     file_put_contents("../" . $path_geojson . "cadremassif.json", $content);
 }
+
+//Fonction permettant de définir un cadre autour des massifs (coordonnées des 4 points)
 function getcadremassif($massif)
 {
     global $hgt_value_size, $hgt_line_records;
@@ -56,9 +68,12 @@ function getcadremassif($massif)
 function generatehgt($massif, $idfile)
 {
     global $hgt_value_size, $hgt_line_size, $hgt_line_records, $hgt_step, $cadremassif, $cadrealpes, $cadrecorse, $cadrepyrenees, $path_maillage, $path_altitude, $fileext, $id_alpes, $id_corse, $id_pyrenees;
+    
+    //Récupération du cadre du massif
     $cadre = getcadremassif($massif);
     $coord = $massif->{"geometry"}->{"coordinates"}[0];
 
+    //Hauteur et largeur en nombre de points
     $height = ceil(($cadre["hd"][0] - $cadre["bg"][0]) * $hgt_line_records);
     $width = ceil(($cadre["hd"][1] - $cadre["bg"][1]) * $hgt_line_records);
 
@@ -78,16 +93,19 @@ function generatehgt($massif, $idfile)
         die("Le massif ".$idfile." n'est pas dans les listes de massifs (alpes, corse, ou pyrénées)");
     }
 
+    //Traduction des coordonnées du polygone d'un massif en lat lon vers indices x y.
     for ($i = 0; $i < count($coord); $i++) {
-        $x = floor((($coord[$i][0] - $cadre["bg"][1]) / ($cadre["hd"][1] - $cadre["bg"][1]) * $width) / 2) * 2;
+        $x = floor((($coord[$i][0] - $cadre["bg"][1]) / ($cadre["hd"][1] - $cadre["bg"][1]) * $width));
         $y = floor((1 - (($coord[$i][1] - $cadre["bg"][0]) / ($cadre["hd"][0] - $cadre["bg"][0]))) * $height);
         array_push($coordxy, [$x, $y]);
     }
 
+    //Numéro des fichiers en bas à gauche et en haut à droite sur lesquels se situe le massif
     $bgfile = [floor($cadre["bg"][0]), floor($cadre["bg"][1])];
     $hdfile = [floor($cadre["hd"][0]), floor($cadre["hd"][1])];
 
     $files = array();
+    //Parcours et ouverture de tous les fichiers sur lesquels se situe le massif 
     for ($n = $bgfile[0]; $n <= $hdfile[0]; $n++) {
         $filesline = array();
         for ($e = $bgfile[1]; $e <= $hdfile[1]; $e++) {
@@ -109,6 +127,7 @@ function generatehgt($massif, $idfile)
         $indexlonhg = floor(($cadre["hg"][1] - floor($cadre["hg"][1])) / $hgt_step);
         $indexhg = $hgt_line_size * $indexlathg + $hgt_value_size * $indexlonhg;
 
+        //On écrit dans le fichier binaire les variables globales du massif
         fseek($fp, 0);
         $valw = fwrite($fp, pack("n", $width), 2);
         fseek($fp, 2);
@@ -122,17 +141,21 @@ function generatehgt($massif, $idfile)
         fseek($fp, 16);
         $valw = fwrite($fp, pack("f", $cadre["hd"][1]), 4);
 
+        //Parcours des fichiers pour isoler le massif
         for ($y = 0; $y < $height; $y++) {
             for ($x = 0; $x < $width; $x++) {
 
+                //Identifiant du fichier
                 $filey = intdiv(intdiv($indexhg, $hgt_line_size) + $y, $hgt_line_size / 2);
                 $filex = intdiv(($indexhg % $hgt_line_size) + ($x * 2), $hgt_line_size);
                 $file = $files[$filey][$filex];
+                //index dans le fichier en question
                 fseek($file, (($indexhg + ($x * 2)) - $filex * $hgt_line_size) + ($y * $hgt_line_size) - $filey * ($hgt_line_size) * ($hgt_line_records + 1));
                 $val = fread($file, 2);
                 $alt = @unpack('n', $val)[1];
 
                 fseek($fp, 20 + $x * $hgt_value_size + $y * $width * $hgt_value_size);
+                //si le point est dans le massif on l'ajoute sinon on ajoute 0
                 if (is_point_in_polygon([$x, $y], $coordxy))
                     $valw = fwrite($fp, pack("n", $alt), 2);
                 else
